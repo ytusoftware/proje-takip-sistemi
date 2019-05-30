37a9207c97bdb2a6baa14bdbadfe1d56f4647d1e
Cetin Tekin <cetin.tekin@alo-tech.com>
2019-05-31 01:23:11 +0300

    process dosyasi olusturuldu

--- main.py
+++ main.py
@@ -14,44 +14,7 @@ from io import BytesIO
 import datetime
 import zipfile
 import time
-
-
-
-#SYSTEM FLOW CONTROL
-####################
-
-#######################################################################################################################
-#ONEMLI NOT: Sadece PROCESS_2 VE PROCESS_3 paralel yurutulebilir. Diger tum surecler birbirinden ayri yurutulmelidir
-#######################################################################################################################
-
-
-#Akademisyen proje oneri gonderme sureci
-#Ogrenci arkadas ekleme sureci
-PROCESS_1 = True
-
-#Ogrenci proje basvuru yapma/silme sureci
-PROCESS_2 = True
-
-#Akademisyen proje basvuru degerlendirme sureci
-PROCESS_3 = True
-
-#Form-2 gonderme sureci
-PROCESS_4 = True
-
-#Form-2 akademisyen onayi sureci
-PROCESS_5 = True
-
-#Form-2 kurul onayi sureci
-PROCESS_6 = True
-
-#Rapor gonderme sureci
-PROCESS_7 = True
-
-
-#Ogrenci devam karari sureci (Bu surec sistem sifirlanana kadar devam etmelidir, bir sonraki donem baslangicina kadar)
-PROCESS_8 = True
-
-
+import process
 
 
 app = Flask(__name__)
